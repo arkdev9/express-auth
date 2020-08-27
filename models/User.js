c7d32221828685b8a1fb1fa4861552d382c30512
Abhishek Kasireddy <akasireddy99@gmail.com>
2020-08-27 17:57:41 +0530

    initialise

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true }
+})
+
+UserSchema.pre('save', function (next) {
+  const user = this
+  // Only hash password if it is changed
+  if (!user.isModified('password')) {
+    return next()
+  }
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err)
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err)
+      user.password = hash
+      next()
+    })
+  })
+})
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err)
+    cb(null, isMatch)
+  })
+}
+
+module.exports = mongoose.model('User', UserSchema)
+
+// Reference on user authentication with Mongoose
+// https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+
+// **Important Note**
+// Mongoose middleware is not invoked on update() operations,
+// so you must use a save() if you want to update user passwords.
---
+++ routes/users.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const User = require('../models/User')
+const debug = require('debug')('shree-express:server')
+
+const router = express.Router()
+
+router.post('/register', (req, res, next) => {
+  // Check if user and password parameters are set
+  if (req.body.username && req.body.password) {
+    // Check if user exists
+    User.findOne({ username: req.body.username })
+      .then(doc => {
+        if (!doc) {
+          // Doesn't exist, create one
+          const newUser = new User({
+            username: req.body.username,
+            password: req.body.password
+          })
+          // We're constructing and saving rather than using
+          // User.create() because our pre-save hook is only
+          // called when we use the save method, and we need
+          // the pre-save hook to hash the password
+          newUser
+            .save()
+            .then(doc => {
+              debug(`Created a new user: ${JSON.stringify(doc, null, 2)}`)
+              res.status(201).json({ message: 'User successfully registered' })
+            })
+            .catch(err => {
+              debug(`Failed to create a new user: ${err}`)
+              res
+                .status(500)
+                .json({ message: `Couldn't register new user: ${err}` })
+            })
+        } else {
+          // A user with the username already exists, return error
+          res
+            .status(400)
+            .json({ message: `${req.body.username} already exists in the DB` })
+        }
+      })
+      .catch(reason => {
+        res.status(500).json({ message: `Internal error ${reason}` })
+      })
+  } else {
+    res.status(401).json({ message: 'Invalid form' })
+  }
+})
+
+router.post('/login', (req, res, next) => {
+  console.log(req.body)
+  if (!req.body.username || !req.body.password) {
+    res.status(401).json({ message: 'No username/password passed' })
+    next()
+  }
+
+  // Validate credentials
+  User.findOne({ username: req.body.username }, (err, doc) => {
+    if (err) res.status(500).json({ message: `Fucking shit myself: ${err}` })
+
+    if (!doc) {
+      res
+        .status(401)
+        .json({ message: "You don't exist on my database asshole" })
+    }
+
+    // Doc found without erros, compare password
+    if (doc.comparePassword(req.body)) {
+      // Password matched
+      res.status(200).json({
+        message: 'Pending implementation but this is where I pass you the JWT'
+      })
+    } else {
+      res.status(401).json({ message: 'Nice try asshole, invalid password' })
+    }
+  })
+})
+
+module.exports = router
