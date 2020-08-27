e50073bd4c29b7dc0e17a55e6485d95f664acee0
Abhishek Kasireddy <akasireddy99@gmail.com>
2020-08-27 18:11:30 +0530

    fixed User model

--- models/User.js
+++ models/User.js
@@ -27,8 +27,8 @@ UserSchema.pre('save', function (next) {
 
 UserSchema.methods.comparePassword = function (candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-    if (err) return cb(err)
-    cb(null, isMatch)
+    if (err) throw err
+    return isMatch
   })
 }
 
